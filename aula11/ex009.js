5fdd9c7dfeb5baf35d0004d8717a0688c91aacd3
MatheusMa10 <matheeeusmatos@gmail.com>
2023-04-24 10:57:28 -0300

    .

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var media = 5
+var nota = 4
+console.log(`Sua nota foi ${nota} da media ${media}`)
+if (nota >= media ) {
+    console.log(`Parabéns! Você Passou. :)`)
+}else {
+    console.log(`Que pena! Você reprovou;. :(`)
+}
